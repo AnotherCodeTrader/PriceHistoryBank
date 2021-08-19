8bdb3debb1241388a93a8577072ee0d2ba1d9f04
Anthony <anthonymichaelroy@gmail.com>
2021-08-19 13:19:32 -0700

    Initial development of encryption/decryption for local storage data

--- TDAmeritrade.Console/HelperMethods.cs
+++ TDAmeritrade.Console/HelperMethods.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Management;
 using System.Net.NetworkInformation;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -22,7 +25,8 @@ namespace TDAmeritrade.Console
         {
             string userAppFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
             string filePath = string.Format("{0}\\{1}.txt", userAppFolder, fileName);
-            string filePathTest = string.Format("{0}\\{1}.txt", userAppFolder, fileName + "Test" + System.Guid.NewGuid());
+            string filePathTest = string.Format("{0}\\{1}.txt", userAppFolder, fileName + "Test");
+            //string filePathTest = string.Format("{0}\\{1}.txt", userAppFolder, fileName + "Test" + System.Guid.NewGuid());
 
             System.Console.WriteLine();
             System.Console.WriteLine(string.Format("Checking Credintial File: {0}", filePath));
@@ -61,6 +65,8 @@ namespace TDAmeritrade.Console
 
             var job = JObject.Parse(jsonRead);
             if (job.ContainsKey("cKey")) _cKey = job["cKey"].ToString();
+            //System.Console.WriteLine("Read from file for encrypted cKey {0}", _cKey);
+            //System.Console.WriteLine("Read from file for decrypted cKey {0}", DecryptCredintialFileValue(_cKey));
 
             CredintialFound = true;
         }
@@ -82,7 +88,7 @@ namespace TDAmeritrade.Console
             _cKey = !_TESTING ? _cKey.Trim() : "SFGJLH48R4VNVLS84HVS8U430TOIKJV9"; // for development testing
 
             // TODO: encrypt _cKey before writing to file with system variable 
-            System.Console.WriteLine(string.Format("Validating cKey Length: {0}", EncryptCredintialFileValue(_cKey)));
+            System.Console.WriteLine(string.Format("Validating cKey Length: {0}", _cKey));
 
             while (!CheckConsumerKey(_cKey))
             {
@@ -90,7 +96,7 @@ namespace TDAmeritrade.Console
                 _cKey = System.Console.ReadLine();
             }
 
-            data.Append(string.Format("\"cKey\":\"{0}\"", _cKey));
+            data.Append(string.Format("\"cKey\":\"{0}\"", EncryptCredintialFileValue(_cKey)));
 
             data.Append("}");
 
@@ -100,27 +106,97 @@ namespace TDAmeritrade.Console
 
         private string EncryptCredintialFileValue(string value)
         {
-            string mac = GetMACAddress();
-            //TODO: encrypt value with mac salt
-            //https://docs.microsoft.com/en-us/dotnet/standard/security/encrypting-data
-            return value;
+            string key = GetManagemenKey();
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(value);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+
+            if (array.Length <= 0) throw new ArgumentException("Encrypted value cannot be empty or null");
+
+            return Convert.ToBase64String(array);
         }
 
         private string DecryptCredintialFileValue(string value)
         {
-            string mac = GetMACAddress();
-            //TODO: decrypt value with mac salt
-            //https://docs.microsoft.com/en-us/dotnet/standard/security/encrypting-data
-            return value;
+            string key = GetManagemenKey();
+
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(value);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            throw new ArgumentException("Decrypted value cannot be empty or null");
         }
 
-        private string GetMACAddress()
+        private string GetManagemenKey()
         {
-            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
-            PhysicalAddress address = nics[0].GetPhysicalAddress();
-            //TODO: Test nics retrieval and mac address;
             //https://docs.microsoft.com/en-us/dotnet/api/system.net.networkinformation.physicaladdress?view=net-5.0
-            return address.ToString();
+
+            string key = ""; // example key "b14ca5898a4e4133bbce2ea2315a1916 length of 32
+
+            //System.Console.WriteLine();
+            ManagementObjectSearcher searcher =
+                new ManagementObjectSearcher("SELECT Manufacturer, Product, SerialNumber FROM Win32_BaseBoard");
+            ManagementObjectCollection information = searcher.Get();
+            foreach (ManagementObject obj in information)
+            {
+                foreach (PropertyData data in obj.Properties)
+                {
+                    //System.Console.WriteLine("{0} = {1}", data.Name, data.Value);
+                    key += data.Value.ToString().Replace(" ", String.Empty).Replace("/", String.Empty).Replace(".", String.Empty);
+                }
+                //System.Console.WriteLine();
+            }
+
+            //System.Console.WriteLine("salt = {0}", key);
+            //System.Console.WriteLine("salt.Length = {0}", key.Length);
+            if (String.IsNullOrEmpty(key)) throw new ArgumentException("salt cannot be empty or null");
+
+            if (key.Length >= 32) return key.Substring(0, 32);
+
+            for(int i = key.Length; i < 32; i++) key += "1";
+
+            //System.Console.WriteLine("salt = {0}", key);
+            //System.Console.WriteLine("salt.Length = {0}", key.Length);
+            return key;
         }
 
         private bool CheckConsumerKey(string line)
